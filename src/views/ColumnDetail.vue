ed87440355680194400132d98e0a1dc371a4baac
BetaCatPro <zj20162325@163.com>
2022-07-25 17:13:12 +0800

    use eslint fix

--- src/views/ColumnDetail.vue
+++ src/views/ColumnDetail.vue
@@ -1,25 +1,27 @@
 <template>
-  <div class="column-detail-page w-75 mx-auto">
-    <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
-      <div class="col-3 text-center">
-        <img :src="column.avatar && column.avatar.fitUrl" :alt="column.title" class="rounded-circle border w-100">
-      </div>
-      <div class="col-9">
-        <h4>{{column.title}}</h4>
-        <p class="text-muted">{{column.description}}</p>
-      </div>
+    <div class="column-detail-page w-75 mx-auto">
+        <div class="column-info row mb-4 border-bottom pb-4 align-items-center" v-if="column">
+            <div class="col-3 text-center">
+                <img :src="column.avatar && column.avatar.fitUrl" :alt="column.title"
+                     class="rounded-circle border w-100">
+            </div>
+            <div class="col-9">
+                <h4>{{column.title}}</h4>
+                <p class="text-muted">{{column.description}}</p>
+            </div>
+        </div>
+        <post-list :list="list"></post-list>
     </div>
-    <post-list :list="list"></post-list>
-  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, computed, onMounted } from 'vue'
+import { computed, defineComponent, onMounted } from 'vue'
 import { useRoute } from 'vue-router'
 import { useStore } from 'vuex'
-import { GlobalDataProps, ColumnProps } from '../store'
+import { ColumnProps, GlobalDataProps } from '../store'
 import PostList from '../components/PostList.vue'
 import { addColumnAvatar } from '../helper'
+
 export default defineComponent({
     components: {
         PostList
--- src/views/Signup.vue
+++ src/views/Signup.vue
@@ -1,46 +1,46 @@
 <template>
-  <div class="signup-page mx-auto p-3 w-330">
-    <h5 class="my-4 text-center">注册者也账户</h5>
-    <validate-form @form-submit="onFormSubmit">
-      <div class="mb-3">
-        <label class="form-label">邮箱地址</label>
-        <validate-input
-          :rules="emailRules" v-model="formData.email"
-          placeholder="请输入邮箱地址"
-          type="text"
-        />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">昵称</label>
-        <validate-input
-          :rules="nameRules" v-model="formData.nickName"
-          placeholder="请输入昵称"
-          type="text"
-        />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">密码</label>
-        <validate-input
-          type="password"
-          placeholder="请输入密码"
-          :rules="passwordRules"
-          v-model="formData.password"
-        />
-      </div>
-      <div class="mb-3">
-        <label class="form-label">重复密码</label>
-        <validate-input
-          type="password"
-          placeholder="请再次密码"
-          :rules="repeatPasswordRules"
-          v-model="formData.repeatPassword"
-        />
-      </div>
-      <template #submit>
-        <button type="submit" class="btn btn-primary btn-block btn-large">注册新用户</button>
-      </template>
-    </validate-form>
-  </div>
+    <div class="signup-page mx-auto p-3 w-330">
+        <h5 class="my-4 text-center">注册者也账户</h5>
+        <validate-form @form-submit="onFormSubmit">
+            <div class="mb-3">
+                <label class="form-label">邮箱地址</label>
+                <validate-input
+                    :rules="emailRules" v-model="formData.email"
+                    placeholder="请输入邮箱地址"
+                    type="text"
+                />
+            </div>
+            <div class="mb-3">
+                <label class="form-label">昵称</label>
+                <validate-input
+                    :rules="nameRules" v-model="formData.nickName"
+                    placeholder="请输入昵称"
+                    type="text"
+                />
+            </div>
+            <div class="mb-3">
+                <label class="form-label">密码</label>
+                <validate-input
+                    type="password"
+                    placeholder="请输入密码"
+                    :rules="passwordRules"
+                    v-model="formData.password"
+                />
+            </div>
+            <div class="mb-3">
+                <label class="form-label">重复密码</label>
+                <validate-input
+                    type="password"
+                    placeholder="请再次密码"
+                    :rules="repeatPasswordRules"
+                    v-model="formData.repeatPassword"
+                />
+            </div>
+            <template #submit>
+                <button type="submit" class="btn btn-primary btn-block btn-large">注册新用户</button>
+            </template>
+        </validate-form>
+    </div>
 </template>
 
 <script lang="ts">
@@ -116,6 +116,6 @@ export default defineComponent({
 
 <style>
 .w-330 {
-  max-width: 330px;
+    max-width: 330px;
 }
 </style>
