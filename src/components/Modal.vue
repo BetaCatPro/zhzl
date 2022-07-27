def521be99229c6446c89168394a97581e3f2815
BetaCatPro <zj20162325@163.com>
2022-07-27 22:12:06 +0800

    update Modal component

---
+++ src/components/Modal.vue
@@ -0,0 +1,47 @@
+<template>
+    <teleport to="#modal">
+        <div class="modal d-block" tabindex="-1" v-if="props.visible">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">{{props.title}}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true" @click="onClose">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <slot></slot>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal" @click="onClose">取消
+                        </button>
+                        <button type="button" class="btn btn-primary" @click="onConfirm">确定</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </teleport>
+</template>
+
+<script lang="ts" setup name="Modal">
+import { defineProps, defineEmits } from 'vue'
+import useDOMCreate from '@/hooks/useDOMCreate'
+
+const props = defineProps({
+    title: String,
+    visible: {
+        type: Boolean,
+        default: false
+    }
+})
+
+const emit = defineEmits(['modal-on-close', 'modal-on-confirm'])
+
+useDOMCreate('modal')
+const onClose = () => {
+    emit('modal-on-close')
+}
+const onConfirm = () => {
+    emit('modal-on-confirm')
+}
+</script>
